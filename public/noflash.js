962f58e2410ede83a724de308cc26f2f25ffef1a
artt <art.tosborvorn@gmail.com>
2024-08-06 21:22:54 +0700

    stuff...

---
+++ public/noflash.js
@@ -0,0 +1,56 @@
+// Adapted from https://github.com/donavon/use-dark-mode/blob/develop/noflash.js.txt
+
+// Insert this script in your index.html right after the <body> tag.
+// This will help to prevent a flash if dark mode is the default.
+
+// Change these if you use something different in your hook.
+
+
+
+
+
+function setRootAttribute(targetTheme) {
+  document.documentElement.setAttribute('data-theme', targetTheme)
+}
+
+var preferDarkQuery = '(prefers-color-scheme: dark)';
+var mql = window.matchMedia(preferDarkQuery);
+var supportsColorSchemeQuery = mql.media === preferDarkQuery;
+var localStorageTheme = null;
+try {
+  localStorageTheme = localStorage.getItem('theme');
+} catch (err) {}
+var localStorageExists = localStorageTheme !== null;
+if (localStorageExists) {
+  localStorageTheme = localStorageTheme;
+}
+
+
+
+// Determine the source of truth
+if (localStorageExists) {
+  // source of truth from localStorage
+  setRootAttribute(localStorageTheme);
+} else if (supportsColorSchemeQuery) {
+  // source of truth from system
+  setRootAttribute(mql.matches ? 'dark' : 'light');
+  localStorage.setItem(storageKey, mql.matches);
+} else {
+  // source of truth from document
+  // var isDarkMode = document.documentElement.setAttribute('data-theme') === 'dark';
+  localStorage.setItem('theme', document.documentElement.getAttribute('data-theme'));
+}
+
+// switchTheme(document.documentElement.getAttribute('data-theme'), 0)
+
+
+
+// // add event listener for data-theme attribute changes
+// const observer = new MutationObserver(function(mutations) {
+//   mutations.forEach(function(mutation) {
+//     if (mutation.type === "attributes" && mutation.attributeName === "data-theme") {
+//       switchTheme(document.documentElement.getAttribute('data-theme'), 300)
+//     }
+//   })
+// })
+// observer.observe(document.documentElement, { attributes: true })
---
+++ src/scripts/darkmode.js
@@ -0,0 +1,43 @@
+
+import { animate } from 'popmotion'
+import { lighten } from 'color2k'
+import { theme } from './theme'
+
+const kebabize = (str) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase())
+
+const switchTheme = (targetTheme, duration) => {
+
+  if (duration === 0) {
+    Object.keys(theme[targetTheme]).forEach((key) => {
+      document.body.style.setProperty(`--color-${kebabize(key)}`, theme[targetTheme][key])
+    })
+  }
+
+  Object.keys(theme[targetTheme]).forEach((key) => {
+    animate({
+      from: document.body.style.getPropertyValue(`--color-${kebabize(key)}`),
+      to: theme[targetTheme][key],
+      duration: duration,
+      onUpdate: (value) => {
+        document.body.style.setProperty(`--color-${kebabize(key)}`, value)
+      }
+    })
+  })
+
+}
+
+switchTheme(document.documentElement.getAttribute("data-theme"), 0)
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.type === "attributes") {
+      if (mutation.attributeName === "data-theme") {
+        switchTheme(mutation.target.getAttribute("data-theme"), 300)
+      }
+    }
+  });
+});
+
+observer.observe(document.documentElement, {
+  attributes: true //configure it to listen to attribute changes
+});
