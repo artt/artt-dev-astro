32ec27c6a36fa2e41980e5a91428ceec2568d2a9
artt <art.tosborvorn@gmail.com>
2024-08-19 00:31:44 +0700

    x

---
+++ src/scripts/lang.ts
@@ -0,0 +1,20 @@
+import { navigate } from 'astro:transitions/client';
+
+function removeTrailingSlash(path: string) {
+  return path === "/" ? path : path.replace(/\/$/, "")
+}
+
+function getLink(to: string, lang: string) {
+  return removeTrailingSlash((lang === "en" ? "/en" : "") + to.replace(/^\/en/, ''))
+}
+
+export function getPreferredLang() {
+  return localStorage.getItem('preferredLang') || 'th';
+}
+
+export function switchLang() {
+  const currentPreferredLang = getPreferredLang()
+  const newPreferredLang = currentPreferredLang === "th" ? "en" : "th"
+  localStorage.setItem("preferredLang", newPreferredLang)
+  navigate(getLink(window.location.pathname, newPreferredLang) || "/")
+}
