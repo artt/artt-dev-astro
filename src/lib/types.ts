a878962e55a34aa6538dbd0fdb7c7a3c0f3742d1
artt <art.tosborvorn@gmail.com>
2024-10-17 23:37:02 +0700

    x

--- src/lib/types.ts
+++
@@ -1,4 +0,0 @@
-import { getCollection } from "astro:content";
-
-export type BlogEntry = Awaited<ReturnType<typeof getCollection<'blog'>>>[number]
-export type PageEntry = Awaited<ReturnType<typeof getCollection<'pages'>>>[number]
