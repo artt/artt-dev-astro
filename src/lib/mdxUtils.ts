caf773b8139364fc168e15d4071d6f2e9f3a08cc
artt <art.tosborvorn@gmail.com>
2024-10-17 11:02:51 +0700

    x

--- src/lib/mdxUtils.ts
+++ src/lib/mdxUtils.ts
@@ -1,15 +1,57 @@
 import { getCollection, type InferEntrySchema } from 'astro:content';
 import { joinPath } from '@/lib/utils';
 
+function getPathPrefix(collection: string): string {
+  return ({
+    blog: 'blog',
+    project: 'project',
+    pages: '',
+  }[collection] || '')
+}
+
 function removeDateFromSlug(slug: string) {
   return slug.replace(/\/\d{2}(?:\.)?\d{2}-/, "/")
 }
+
+// export async function getAllBlogPosts(lang: string, maxPosts: number = 0) {
+  
+//   const blogEntries = await getCollection('blog')
+//   const allBlogs = blogEntries.map((entry) => {
+//     const ret = generateMainSlugFromEntry(entry, 'blog/')
+//     if (!ret) return
+//     const { mainSlug, isBilingual, lang: entryLang } = ret
+//     // check the length of the processedEntry array
+//     // if it's 1, then it's a bilingual page
+//     // if it's 2, then it's a single language page (the other one is a redirect)
+//     if (isBilingual) {
+//       // it's a bilingual page; check if the entry's language matches the current locale
+//       // if the language matches, return the props; otherwise return null and wait for the correct one
+//       if (entryLang === lang) {
+//         return processedEntry[0].props
+//       }
+//       return null
+//     }
+//     else {
+//       return processedEntry[0].props
+//     }
+//   })
+  
+//   // filter out the null values and sort by entry.data.date in descending order
+//   const sortedBlogs = allBlogs.filter((entry) => entry !== null).sort((a, b) => {
+//     return (b.entry?.data as InferEntrySchema<"blog">).date.getTime() - (a.entry?.data as InferEntrySchema<"blog">).date.getTime()
+//   })
+
+//   if (maxPosts > 0) {
+//     return sortedBlogs.slice(0, maxPosts)
+//   }
+//   return sortedBlogs
+// }
 
 export async function getAllBlogPosts(lang: string, maxPosts: number = 0) {
   
   const blogEntries = await getCollection('blog')
   const allBlogs = blogEntries.map((entry) => {
-    const processedEntry = generateStaticPathFromEntry(entry, 'blog/')
+    const processedEntry = generateStaticPathFromEntry(entry)
     // check the length of the processedEntry array
     // if it's 1, then it's a bilingual page
     // if it's 2, then it's a single language page (the other one is a redirect)
@@ -37,21 +79,14 @@ export async function getAllBlogPosts(lang: string, maxPosts: number = 0) {
   return sortedBlogs
 }
 
-
-
-export function generateStaticPathFromEntry (
-  entry: Awaited<ReturnType<typeof getCollection>>[number],
-  collection: string
-) {
-
-  // test if entry.filePath ends with .(en|th).mdx?
+// Transforms an entry (from )
+export function generateMainSlugFromEntry(entry: Awaited<ReturnType<typeof getCollection>>[number]) {
   if (!entry.filePath)
-    return []
-
+    return null
   const matchBilingual = entry.filePath.match(`content\/${entry.collection}\/(?:(.*?)\/)?([^\/]*?)\.(th|en)\.mdx?$`)
-
-  let mainSlug = ""
-
+  let mainSlug
+  let isBilingual
+  let lang
   if (matchBilingual) {
     // if this is the case then we assume that there's no need to redirect anything
     // since *.th.mdx and *.en.mdx must exist in pair
@@ -61,23 +96,39 @@ export function generateStaticPathFromEntry (
     }
     else {
       // otherwise, mainSlug should be matchBilingual[1] + "/" + matchBilingual[2]
-      mainSlug = `/${matchBilingual[1]}/${matchBilingual[2]}`
+      mainSlug = `${matchBilingual[1]}/${matchBilingual[2]}`
     }
-    mainSlug = joinPath([collection, removeDateFromSlug(mainSlug)])
+    mainSlug = joinPath([getPathPrefix(entry.collection), removeDateFromSlug(mainSlug)])
+    isBilingual = true
+    lang = matchBilingual[3]
+  }
+  else {
+    mainSlug = joinPath([getPathPrefix(entry.collection), removeDateFromSlug(entry.id)])
+    isBilingual = false
+    // if lang is "th" or not specified, then we assume that it's Thai
+    lang = entry.data.lang === "en" ? "en" : "th"
+  }
+  return { mainSlug, isBilingual, lang }
+}
+
+export function generateStaticPathFromEntry(entry: Awaited<ReturnType<typeof getCollection>>[number]) {
+
+  const ret = generateMainSlugFromEntry(entry)
+  if (!ret) return []
+  const { mainSlug, isBilingual, lang } = ret
+
+  if (isBilingual) {
     return [{
-      params: { dynamicMdxPath: joinPath([matchBilingual[3] === "en" ? "en" : "", mainSlug], false) },
-      props: { entry, mainSlug, lang: matchBilingual[3], isBilingual: true },
+      params: { dynamicMdxPath: joinPath([lang === "en" ? "en" : "", mainSlug], false) },
+      props: { entry, mainSlug, lang, isBilingual: true },
     }]
   }
   else {
-    mainSlug = joinPath([collection, removeDateFromSlug(entry.id)])
-    // mainSlug = removeDateFromSlug(mainSlug)
-    // cannot infer language from file name, so try to see if it's specified in the frontmatter
-    if (entry.data.lang === "en") {
+    if (lang === "en") {
       return [
         {
           params: { dynamicMdxPath: joinPath(['en', mainSlug], false) },
-          props: { entry, mainSlug, lang: "en", isBilingual: false },
+          props: { entry, mainSlug, lang, isBilingual: false },
         },
         {
           params: { dynamicMdxPath: joinPath([mainSlug], false) },
@@ -86,11 +137,10 @@ export function generateStaticPathFromEntry (
       ]
     }
     else {
-      // if lang is "th" or not specified, then we assume that it's Thai
       return [
         {
           params: { dynamicMdxPath: joinPath([mainSlug], false) },
-          props: { entry, mainSlug, lang: "th", isBilingual: false },
+          props: { entry, mainSlug, lang, isBilingual: false },
         },
         {
           params: { dynamicMdxPath: joinPath(['en', mainSlug], false) },
