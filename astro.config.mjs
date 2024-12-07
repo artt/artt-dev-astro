936765cbb458a6d2f5960847e96b9503fd8406e9
artt <art.tosborvorn@gmail.com>
2024-12-08 01:30:34 +0700

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,10 +43,10 @@ const defaultLocale = "th"
 // https://astro.build/config
 export default defineConfig({
   site: "https://artt.dev",
-  trailingSlash: "always",
-  build: {
-    format: "directory",
-  },
+  // trailingSlash: "always",
+  // build: {
+  //   format: "directory",
+  // },
   output: 'static',
   adapter: vercel({
     // imageService: true,
