fa7d78eb1ed11e071ec4567a82377c7a93e7c46b
artt <art.tosborvorn@gmail.com>
2024-12-08 00:47:34 +0700

    back to pic

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import sitemap from "@astrojs/sitemap";
-import { imageService } from "@unpic/astro/service";
+// import { imageService } from "@unpic/astro/service";
 
 import remarkHeadingId from "remark-custom-heading-id"
 import rehypeSlug from "rehype-slug"
@@ -65,14 +65,7 @@ export default defineConfig({
     ],
   },
   image: {
-    // service: imageService({
-    //   cdnOptions: {
-    //     astro: {
-    //       trailingSlash: true,
-    //     },
-    //   },
-    // }),
-    service: imageService(),
+    // service: imageService(),
   },
   integrations: [
     tailwind({
