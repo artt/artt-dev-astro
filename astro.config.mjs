78c7a07386acd99cf652ee8f9424e7d88c3779d6
artt <art.tosborvorn@gmail.com>
2025-01-14 09:40:31 +0700

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import sitemap from "@astrojs/sitemap";
-import { imageService } from "@unpic/astro/service";
+// import { imageService } from "@unpic/astro/service";
 
 import remarkHeadingId from "remark-custom-heading-id"
 import rehypeSlug from "rehype-slug"
@@ -69,8 +69,8 @@ export default defineConfig({
   },
 
   image: {
-    domains: ["https://artt.dev", "https://www.artt.dev"],
-    service: imageService(),
+    // domains: ["https://artt.dev", "https://www.artt.dev"],
+    // service: imageService(),
   },
 
   integrations: [
