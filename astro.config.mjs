51515ac48c1b146d7768f3c5d47570ff76a71fb5
artt <art.tosborvorn@gmail.com>
2025-01-14 09:56:04 +0700

    clean

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import sitemap from "@astrojs/sitemap";
-import { imageService } from "@unpic/astro/service";
+// import { imageService } from "@unpic/astro/service";
+// import netlify from "@astrojs/netlify";
 
 import remarkHeadingId from "remark-custom-heading-id"
 import rehypeSlug from "rehype-slug"
@@ -69,7 +70,7 @@ export default defineConfig({
 
   image: {
     // domains: ["https://artt.dev", "https://www.artt.dev"],
-    service: imageService(),
+    // service: imageService(),
   },
 
   integrations: [
@@ -126,4 +127,5 @@ export default defineConfig({
     }
   },
 
+  // adapter: netlify(),
 });
